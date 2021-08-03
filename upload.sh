b406bc3793a8f1bded28a03a5ac22089a1796dfc
gxhx <gxhx2006@foxmail.com>
2021-08-03 17:29:53 +0800

    s.version          = '0.1.0'

---
+++ upload.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+Cyan='\033[0;36m'
+Default='\033[0;m'
+
+podSpecName=""
+podVersionName=""
+confirmed="n"
+confirmed1=""
+VersionString = ""
+podspecTag = ""
+
+
+getPodspecName() {
+    read -p "Enter Podspec Name: " podSpecName
+
+    if test -z "$podSpecName"; then
+        podSpecName
+    fi
+}
+getPodspecVersion() {
+    read -p "Enter Podspec Version: " podVersionName
+
+    if test -z "$podVersionName"; then
+        podVersionName
+    fi
+}
+getPodspecTag() {
+    read -p "Enter new tag: " podspecTag
+
+    if test -z "$podspecTag"; then
+        podspecTag
+    fi
+}
+
+
+getInfomation() {
+    getPodspecName
+    getPodspecTag
+    echo -e "\n${Default}================================================"
+    echo -e "  Podspec Name  :  ${Cyan}${podSpecName}.podspec${Default}"
+    echo -e "  Project tag    :  ${Cyan}${podspecTag}${Default}"
+
+    echo -e "================================================\n"
+}
+#用户判断
+echo -e "\n"
+while [ "$confirmed" != "y" -a "$confirmed" != "Y" ]
+do
+    if [ "$confirmed" == "n" -o "$confirmed" == "N" ]; then
+        getInfomation
+    fi
+    read -p "confirm? (y/n):" confirmed
+done
+
+
+
+VersionString=`grep -E 's.version.*=' ${podSpecName}.podspec`
+
+echo -e "\n${Default}================================================"
+echo -e "  Podspec Version  :  ${Cyan}$VersionString${Default}"
+echo -e "================================================\n"
+
+#用户判断
+echo -e "\n"
+while [ "$confirmed1" != "y" -a "$confirmed1" != "Y" ]
+do
+
+#    if [ "$confirmed1" == "n" -o "$confirmed1" == "N" ]; then
+#        getPodspecVersion
+#    fi
+    read -p "confirm? (y/n):" confirmed1
+done
+
+
+echo "current version is ${VersionString}"
+
+git stash
+git pull origin master --tags
+git stash pop
+
+git add .
+git commit -a -m "${VersionString}"
+git tag "${podspecTag}" -m "${VersionString}"
+git push --tags
+git push origin master
+set the new version to ${podspecTag}
+set the new tag to ${podspecTag}
+
+
+cd ~/.cocoapods/repos/GXSpec && git pull origin master && cd - && pod repo push GXSpec ${podSpecName}.podspec --verbose --allow-warnings --use-libraries --skip-import-validation
